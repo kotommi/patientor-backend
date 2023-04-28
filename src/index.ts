8e72df4584b271cab5ac67742ccbe00892d4d4a6
Tommi Koivula <tommi.koivula93@gmail.com>
2023-04-28 15:34:56 +0300

    add cors for frontend

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from "cors";
 import diagRouter from './routes/diagnosis';
 import patientRouter from "./routes/patient";
 
 const app = express();
 app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 
 const PORT = 3000;
 
